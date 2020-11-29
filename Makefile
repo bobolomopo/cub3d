9868b9586134ea548f27525052afe8da918242cf
jandre <jandre@student.42.fr>
2020-11-29 16:45:31 +0100

    premiers tests

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: jandre <marvin@42.fr>                      +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/11/25 17:21:03 by jandre            #+#    #+#              #
-#    Updated: 2020/11/25 21:18:08 by jandre           ###   ########.fr        #
+#    Updated: 2020/11/29 16:00:49 by jandre           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,6 +15,7 @@ CC = gcc
 FLAGS = -Wall -Wextra -Werror
 LIB = -I/usr/local/include -L/usr/local/lib -lmlx -L/usr/include -lm -lbsd -lX11 -lXext
 SRC = test.c
+INC = /include
 OBJ = $(SRC:.c=.o)
 
 all: $(NAME)
@@ -23,7 +24,7 @@ $(NAME):
 	$(CC) $(SRC) $(LIB) -o $(NAME)
 
 %.o: %.c
-	$(CC) $(FLAGS) $(INC) -o $@ -c $<
+	$(CC) $(FLAGS) -I $(INC) -o $@ -c $<
 
 clean:
 	sudo rm -rf *.o
--- test.c
+++ test.c
@@ -6,42 +6,42 @@
 /*   By: jandre <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/25 17:36:04 by jandre            #+#    #+#             */
-/*   Updated: 2020/11/25 17:51:21 by jandre           ###   ########.fr       */
+/*   Updated: 2020/11/29 16:44:35 by jandre           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <mlx.h>
-#include <stdio.h>
+#include "include/cub3D.h"
 
-typedef struct  s_data {
-	void        *img;
-	char        *addr;
-	int         bits_per_pixel;
-	int         line_length;
-	int         endian;
-}               t_data;
-
-void            my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void            my_mlx_pixel_put(t_img *img, int x, int y, int color)
 {
-	char    *dst;
+    char    *dst;
 
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
+    dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+    *(unsigned int*)dst = color;
 }
 
-int             main(void)
+int main()
 {
-	void    *mlx;
-	void    *mlx_win;
-	t_data  img;
+	t_display	dis;
+	t_img		img;
+	int		x;
+	int		y;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-	img.img = mlx_new_image(mlx, 1920, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-			&img.endian);
+
+	dis.mlx = mlx_init();
+	dis.win = mlx_new_window(dis.mlx, 800, 640, "Cub3D");
+	img.img = mlx_new_image(dis.mlx, 800, 640);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
+		&img.line_length, &img.endian);
+	x = 100;
+	while (x++ <= 200)
+	{
+		y = 100;
+		while (y++ <= 200)
+			my_mlx_pixel_put(&img, x, y, 0X00FF0000);
+	}
 	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx);
+	mlx_put_image_to_window(dis.mlx, dis.win, img.img, 0, 0);
+	mlx_loop(dis.mlx);
+	return (0);
 }
-
