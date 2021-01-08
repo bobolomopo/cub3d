893e89f488a1334125ced5a3eadb0b04b8984097
Julien Andre <jandre@shi-r8-p1.s19.be>
2021-01-08 14:49:12 +0100

    commentaire et corr

--- parsing/fill_param_map.c
+++ parsing/fill_param_map.c
@@ -12,6 +12,10 @@
 
 #include "../include/cub3D.h"
 
+/*
+**on verifie si la ligne n'est composee que de caracteres autorise dans la map
+*/
+
 static int		verif(char *line)
 {
 	int		i;
@@ -25,6 +29,11 @@ static int		verif(char *line)
 	}
 	return (1);
 }
+
+/*
+**on remplit la parametre map et on oublie pas de free les str qui ont ete
+**allouee
+*/
 
 static int		fill_map(char **map, t_param *param)
 {
@@ -49,51 +58,73 @@ static int		fill_map(char **map, t_param *param)
 	return (1);
 }
 
-static int		valid_c(char **map, int i, int j, int start)
+/*
+**on verifie si le caractere est bien autorise et si la map est fermee en
+**verifiant tout les caractere quie l'entoure, il ne peut etre entoure que
+**par un mur ou alors par un autre espace vide, sinon la map n'est pas
+**valide
+*/
+
+static int		valid_c(char **map, int *i, t_param *param)
 {
-	if (ft_isin(map[i][j], "NESW") > 0)
+	if (ft_isin(map[i[0]][i[1]], "NESW") > 0)
 	{
-		if (start == 1)
+		if (i[2] == 1)
 			return (-1);
-		start = 1;
+		param->pos_x = i[0];
+		param->pos_y = i[1];
+		direction(map[i[0]][i[1]], param);
+		i[2] = 1;
 	}
-	if (!(map[i][j + 1]) || (ft_isin(map[i][j + 1], "012NESW") < 0))
+	if (!(map[i[0]][(i[1]) + 1]) ||
+		(ft_isin(map[i[0]][(i[1]) + 1], "012NESW") < 0))
 		return (-1);
-	if (!(map[i][j - 1]) || (ft_isin(map[i][j - 1], "012NESW") < 0))
+	if (!(map[i[0]][(i[1]) - 1]) ||
+		(ft_isin(map[i[0]][(i[1]) - 1], "012NESW") < 0))
 		return (-1);
-	if (!(map[i + 1][j]) || (ft_isin(map[i + 1][j], "012NESW") < 0))
+	if (!(map[(i[0]) + 1][i[1]]) ||
+		(ft_isin(map[(i[0]) + 1][i[1]], "012NESW") < 0))
 		return (-1);
-	if (!(map[i - 1][j]) || (ft_isin(map[i - 1][j], "012NESW") < 0))
+	if (!(map[(i[0]) - 1][i[1]]) ||
+		(ft_isin(map[(i[0]) - 1][i[1]], "012NESW") < 0))
 		return (-1);
 	return (1);
 }
 
+/*
+**verification si la map est ouverte, en deux partie avec la fonction
+**statique valic_c
+*/
+
 static int		verif_open(char **map, t_param *param)
 {
-	int		i;
-	int		j;
-	int		start;
+	int		i[3];
 
-	start = 0;
-	i = 0;
-	while (map[i])
+	i[2] = 0;
+	i[0] = 0;
+	while (map[i[0]])
 	{
-		j = 0;
-		while (j < ft_strlen(map[i]))
+		i[1] = 0;
+		while (i[1] < ft_strlen(map[i[0]]))
 		{
-			if (ft_isin(map[i][j], " 012NESW") < 0)
+			if (ft_isin(map[i[0]][i[1]], " 012NESW") < 0)
 				return (parsing_error(NULL, -7));
-			if (ft_isin(map[i][j], "02NESW") > 0)
+			if (ft_isin(map[i[0]][i[1]], "02NESW") > 0)
 			{
-				if (valid_c(map, i, j, start) < 0)
+				if (valid_c(map, i, param) < 0)
 					return (parsing_error(NULL, -7));
 			}
-			j++;
+			i[1]++;
 		}
-		i++;
+		i[0]++;
 	}
 	return (fill_map(map, param));
 }
+
+/*
+**fonction principale pour remplir la parametre de map, va remplir
+**le param map, les directino et positions de depart du joueur
+*/
 
 int				fill_param_map(int fd, t_param *param)
 {
